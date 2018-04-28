4907472455754fdd718b676d4037f6676d5cfcfe
sophiaberger <sophia.alexandra.berger@gmail.com>
2018-04-28 17:30:52 -0400

    added js, css, menu

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
 // Create a "close" button and append it to each list item
+/*
+
 var myNodelist = document.getElementsByTagName("LI");
 //myNodeList = document.getElementsByTagName("addBtn");
 var i;
@@ -11,50 +13,213 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
+
+// load everything from local storage
+	var v = [];
+	//if localStorage exists, parse data
+	if (localStorage.foodList){
+	v = JSON.parse(localStorage.foodList);
+	}
+	else{
+		localStorage.foodList = JSON.stringify(v);
+	}
+	
+	//output lisy
+	var str = "";
+	for (var i = 0; i < v.length; i++){
+		str += ('<ul id = "myUL">');
+		str += ('<li id = "close">');
+		str += v[i].foodName + ": Expires on " + v[i].expirationDate ;
+		str += ('<br />');
+		str += ('</li>');
+		str += ('</ul>');
+		console.log(str);
+	}
+	$("#result").html(str);
+	//$("#result").html('<br />');
+	
+	
+	
+
+
+
+*/
+
 // Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
+    //ev.target.classList.toggle("close");
+    
   }
 }, false);
 
+
+
+
+var food;
+
+
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
+
+	
+  //var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
+  var date = document.getElementById("date").value; //added by me 
+  //console.log("THE SHIT IS "+date+" - "+inputValue);
+  //var epoch = Date.now();
+  
+  
+  //yada 
+  
+  console.log("clicked it");
+  v = [];
+  //if it doesn't exist yet 
+  if (!localStorage.foodList){
+  localStorage.foodList = JSON.stringify(v);
+  }
+  v = JSON.parse(localStorage.foodList);
+  
+  
+    
+// get the old list
+// append the new shit
+// save the new list
+  
+  //var t = document.createTextNode(inputValue);
+  //var t3 = document.createTextNode(": Expires on "); //added by me
+  //var t2 = document.createTextNode(date); //added by me
+  //li.appendChild(t);
+  //li.appendChild(t3); //added by me
+  //li.appendChild(t2);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    //document.getElementById("myUL").appendChild(li);
+    //var txt.attribute = "\u2610";
+    //txt.id = "close";
+    food = {
+    "expirationDate": date,
+     "foodName": inputValue,
+      id: v.length};
+      
+    
+  	v.push(food);
+  	
+  //v.push(span);
+  	localStorage.foodList = JSON.stringify(v);
+  	loadFoods();
   }
-  document.getElementById("myInput").value = "";
+  //document.getElementById("myInput").value = "";
+  //document.getElementById("date").value = ""; //added by me
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u2610");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
+  //var span = document.createElement("SPAN");
+  
+  //txt.setAttribute("close", "\u2610");
+  //span.className = "close";
+  //span.appendChild(txt);
+  //li.appendChild(span);
+  
+  
+  
+   //(putting the close button on)
+  //listContents.push(li); //added by me
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function() { //when closed
       var div = this.parentElement;
       div.style.display = "none";
     }
+    
   }
+
+  
+  // Check browser support
+	
+
+	
+	
+  sortListDir();
 }
+
+
+
+$(document).ready(function() {
+
+console.log("LOADED");
+	//var list = $("#myUL");
+	//var listContents = [];
+
+   loadFoods(); 
+});
+
+function loadFoods(){
+	
+// load everything from local storage
+	var v = [];
+	//if localStorage exists, parse data
+	if (localStorage.foodList){
+	v = JSON.parse(localStorage.foodList);
+	}
+	else{
+		localStorage.foodList = JSON.stringify(v);
+	}
+	
+	//output lisy
+	var str = "";
+	for (var i = 0; i < v.length; i++){
+		str += ('<ul id = "myUL">');
+		str += ('<li>');
+		str += v[i].foodName + ": Expires on " + v[i].expirationDate ;
+		str += ('<br />');
+		str += ('</li>');
+		str += ('</ul>');
+		console.log(str);
+	}
+	$("#result").html(str);
+	//$("#result").html('<br />');
+	
+	
+	
+	
+	
+}
+
+
+/*
+if (typeof(Storage) !== "undefined") {
+	var b, list, i = 0;
+    // Store
+    list = document.getElementById("myUL");
+    b = list.getElementsById("LI");
+    for (i = 0; i < (b.length - 1); i++) {
+    localStorage.setItem("food", b[i]);
+    }
+    //localStorage.setItem("food", list.getElementById("keep"));
+    // Retrieve
+    
+    
+    document.getElementById("result").innerHTML = localStorage.food;
+    
+    
+} else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+	}
+	*/
+	
+function menu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
 
 function sortListDir() {
   var list, i, switching, b, shouldSwitch, dir, switchcount = 0;
@@ -66,7 +231,8 @@ function sortListDir() {
   while (switching) {
     //start by saying: no switching is done:
     switching = false;
-    b = list.getElementsByTagName("LI");
+    b = $("#date");
+    
     //Loop through all list-items:
     for (i = 0; i < (b.length - 1); i++) {
       //start by saying there should be no switching:
@@ -74,14 +240,14 @@ function sortListDir() {
       /*check if the next item should switch place with the current item,
       based on the sorting direction (asc or desc):*/
       if (dir == "asc") {
-        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        if (b[i].innerHTML.getDate > b[i + 1].innerHTML.getDate()) {
           /*if next item is alphabetically lower than current item,
           mark as a switch and break the loop:*/
           shouldSwitch= true;
           break;
         }
       } else if (dir == "desc") {
-        if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
+        if (b[i].innerHTML.getDate() < b[i + 1].innerHTML.getDate()) {
           /*if next item is alphabetically higher than current item,
           mark as a switch and break the loop:*/
           shouldSwitch= true;
